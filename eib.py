5856ac5044a604c10f9e7405207af3f734157a87
Margie Athol <atholm@kenyon.edu>
2019-05-06 20:36:25 -0400

    upload twitter, eib, & rnn
    uploading the final project files

--- eib.py
+++ eib.py
@@ -1,15 +1,12 @@
-# this file will do the following:
+# this file contains function definitions to do the following:
 # 1. import an image from twitter.py
-# 2. analyse the image
+# 2. "analyse" the image
 # 3. choose a seed word
 
 import random
-# from twitter import filename
-# image = filename
-
-word1 = 'yes'
-word2 = 'yes'
-word3 = 'yes'
+import PIL
+from PIL import Image
+from PIL import ImageFile
 
 # Defining the lists for each color category that will hold the possible seed words
 red = ["fire", "hot", "anger", "angry", "mad", "apple", "candy", "stop", "passion", "celebration", "evil", "caution", "prosperity", "luck", "energy", "desire"]
@@ -25,31 +22,122 @@ black = ["darkness", "unknown", "mystery", "finality", "death", "force", "contro
 
 # Defining the function that tells us into which color category the RGB code falls
 def which_color(R,G,B):
-    if (R == 255 & G <= R & B <= R) or (G == 0 & B == 0):
-        colorname = red
-    elif (R < 255 & R >= 200 & R >= G & G >= B & G != 0 & B != 0):
-        colorname = orange
-    elif (B < R & R == G):
-        colorname = yellow
-    elif (G == 255) or (R == 0 & B == 0) or (G > B & G > R):
-        colorname = green
-    elif (B == 255 & G >= R) or (R == 0 & G == 0):
-        colorname = blue
-    elif (G == 0 & R != 0 & R == 0.5*B) or (G == 0 & R != 0 & R == 0.5*(B-1)) or (G == R & B >= R):
-        colorname = purple
-    elif (R < 200 & R > G & G > B):
-        colorname = brown
-    elif (R == G & G == B & R >= 195):
-        colorname = white
-    elif (R == G & G == B & R >= 66 & R <= 194):
-        colorname = grey
-    elif (R == G & G == B & R <= 65):
-        colorname = black
+    if (R < 200 and R > G and G > B):           # each if statement will test to see into which category
+        colorname = "brown"                     # the color at hand falls.
+    elif (B >= 235 and G >= 235 and R >= 235):  # all RGB ranges are based on my own observations and testing.
+        colorname = "white"
+    elif (R >= 66 and R < 235) and ( B >= 66 and B < 235) and (G >= 66 and G < 235):
+        colorname = "grey"
+    elif (G <= 65 and B <= 65 and R <= 65):
+        colorname = "black"
+    elif (R == 255 and G < R and B < R) or (G == 0 and B == 0): # make more categories for orange and red
+        colorname = "red"
+    elif (R < 255 and R >= 20) and (R >= G and G >= B ) and (G != 0 and B != 0):
+        colorname = "orange"
+    elif (B < R and R == G):
+        colorname = "yellow"
+    elif (G == 255 and G>R and G>B) or (R == 0 and B == 0) or (G > B and G > R):
+        colorname = "green"
+    elif (B == 255 and G >= R) or (R == 0 and G == 0):
+        colorname = "blue"
+    elif (G == 0 and R != 0 and R == 0.5*B) or (G == 0 and R != 0 and R == 0.5*(B-1)) or (G == R and B >= R):
+        colorname = "purple"
     else:
         print ("no color found")
         return;
     print (colorname)
     return colorname;
+
+def most_frequent(List):
+    result = max(set(List), key=List.count)     # this function was adapted from one I found online
+    print ("printing the result")               # it will work for any input int, char, or string list
+    print(result)
+    return result
+
+def word_to_list(colorword):     # this function will convert a given color to the color list
+    if colorword == "red":
+        return red
+    elif colorword == "orange":
+        return orange
+    elif colorword == "yellow":
+        return yellow
+    elif colorword == "green":
+        return green
+    elif colorword == "blue":
+        return blue
+    elif colorword == "purple":
+        return purple
+    elif colorword == "brown":
+        return brown
+    elif colorword == "white":
+        return white
+    elif colorword == "grey":
+        return grey
+    elif colorword == "black":
+        return black
+
+
+def find_color_1(im, width, height):
+    rgb_im = im.convert('RGB')              # convert image to RGB
+    # Go through the image's pixels
+    colors = []                             # initialize an empty list
+    for i in range(width):
+        for j in range(0, height//3):       # this height range is so that we only look at the bottom third for now
+            p = rgb_im.getpixel((i, j))     # get the pixel we are looking at
+            print("printing p")
+            print(p)
+            this_color = which_color(p[0],p[1],p[2])    # find what color it is
+            print("printing this_color")
+            print(this_color)
+            colors.append(this_color)       # add the color to the list
+    the_color = most_frequent(colors)       # find the most frequent color
+    print("printing the_color")
+    print(the_color)
+    the_color_list = word_to_list(the_color) # convert the color to a seed word
+    print(the_color_list)
+    return the_color_list                    # return the seed word
+
+
+# the next to functions do the same thing but for the other thirds of the photo
+def find_color_2(im, width, height):
+    rgb_im = im.convert('RGB')              # convert image to RGB
+    # Go through the image's pixels
+    colors = []                                  # this will be the index for the list of colors in the photo
+    for i in range(width):
+        for j in range((height//3)+1, 2*(height//3)):
+            p = rgb_im.getpixel((i, j))
+            print("printing p")
+            print(p)
+            this_color = which_color(p[0], p[1], p[2])
+            print("printing this_color")
+            print(this_color)
+            colors.append(this_color)
+        the_color = most_frequent(colors)
+        print("printing the_color")
+        print(the_color)
+        the_color_list = word_to_list(the_color)
+        print(the_color_list)
+        return the_color_list
+
+def find_color_3(im, width, height):
+    rgb_im = im.convert('RGB')              # convert image to RGB
+    # Go through the image's pixels
+    colors = []                                    # this will be the index for the list of colors in the photo
+    for i in range(width):
+        for j in range((2*(height//3))+1,height):
+            p = rgb_im.getpixel((i, j))
+            print("printing p")
+            print(p)
+            this_color = which_color(p[0], p[1], p[2])
+            print("printing this_color")
+            print(this_color)
+            colors.append(this_color)
+        the_color = most_frequent(colors)
+        print("printing the_color")
+        print(the_color)
+        the_color_list = word_to_list(the_color)
+        print(the_color_list)
+        return the_color_list
 
 # defining the function that will randomly pick one of the associated seed words from the color category vector entered as input
 def pick_a_word(color):
@@ -58,17 +146,16 @@ def pick_a_word(color):
     for x in range(1):                              # pick one number
         number = random.randint(0,length-1)         # in the range 0 - length-1 (number will be used as an index)
     word = color[number]
+    print("printing word")
+    print(word)
     return word;
 
-# def'n for function to go from one complete image to three seed words ----- just testing this for now
-# steps of the function
-# 1. split image into thirds
-# 2. identify most used color in each third
-# 3. pick seed word for each third
-def image_to_seeds(color):
-    global word1, word2, word3
-    word1 = pick_a_word(color)
-    return
 
-image_to_seeds(green)
-print(word1)
+# everything after this is for testing the functions
+if __name__ == "__main__": # this idiom is useful so that when importing this file, anything after this line won't be run
+# testing which_color
+    which_color(50, 50, 50)
+# testing the most frequent function
+    testlist = "dog", "cat", "cat", "goose"
+    most_frequent(testlist)
+
--- twitter.py
+++ twitter.py
@@ -2,33 +2,30 @@
 #   after passing information to and receiving information from other files
 
 # import everything we will need
-
 import tweepy
 from secrets import *
 import time
 from io import BytesIO
 import requests
-from PIL import Image
-from PIL import ImageFile
-
+from rnn import *
+from eib import *
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 # start an OAuthHandler Instance
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
 # definitions for functions that will store the tweet id of the last tweet that the bot saw
-FILE_NAME = 'last_seen_id.txt' # this makes it so I can easily write to the file
-
-def retrieve_last_seen_id(file_name): #this function will retrieve the last seen id which is the text within the file
+FILE_NAME = 'last_seen_id.txt'  # this makes it so I can easily write to the file
+def retrieve_last_seen_id(file_name):  # this function will retrieve the last seen id which is the text within the file
     f_read = open(file_name, 'r')
     last_seen_id = int(f_read.read().strip())
     f_read.close()
     return last_seen_id
 
-def store_last_seen_id(last_seen_id, file_name): #this function will edit the file and update it with the most recently seen tweet id
+def store_last_seen_id(last_seen_id,
+                       file_name):  # this function will edit the file and update it with the most recently seen tweet id
     f_write = open(file_name, 'w')
     f_write.write(str(last_seen_id))
     f_write.close()
@@ -38,29 +35,29 @@ def store_last_seen_id(last_seen_id, file_name): #this function will edit the fi
 filename = 'yourimage.png'
 last_seen_id = retrieve_last_seen_id(FILE_NAME)
 
-# def'n for function that will download and output the image tweeted at the account
+# def'n for function that will download the image tweeted at the account
 def dwn_img():
-    global filename
+    global filename                     # bring in those global variables
     filename = 'yourimage.png'
     print('retrieving tweets...')
     global last_seen_id
-    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)        # get the last seen tweet id to start working with the tweet
     print('last seen id is: ')
     print(last_seen_id)
     mentions = api.mentions_timeline(
-                        last_seen_id,
-                        tweet_mode='extended',
+                           last_seen_id,
+                           tweet_mode='extended',
                         include_entities=True) #this will set mentions equal to all mentions since the last seen mention
     print(mentions)
     for mention in reversed(mentions): #we reverse mentions so that we go through the tweets in chronological order
         last_seen_id = mention.id
-        store_last_seen_id(last_seen_id, FILE_NAME)
+
 
         if 'media' in mention.entities:
             print('found an image')
             photo_URL = mention.entities['media'][0]['media_url']
             print(photo_URL)
-                # download image
+            # download image:
             request = requests.get(photo_URL, stream=True) # send a get request to get the image
             i = Image.open(BytesIO(request.content))
             i.save(filename)
@@ -71,26 +68,64 @@ def dwn_img():
             return
 
 
-# import rnn file and set it to global variables
-import rnn
-
-line1 = rnn.line1
-line2 = rnn.line2
-line3 = rnn.line3
-
-print('printing the poetry...')
-print(line1 + '\n' + line2 + '\n' + line3)
-
 # def'n for function that will tweet the image and lines of poetry
-def tweet_img():
+def tweet_img(filename, line1, line2, line3):       # input the image filename and the 3 lines to be tweeted
     print('sending poetry and image...')
-    global last_seen_id, filename, line1, line2, line3
-    api.update_with_media(filename, status='@'+ mention.user.screen_name + '\n' + line1 +'\n'+line2+'\n'+line3,
-                                    in_reply_to_status_id=last_seen_id)
+    last_seen_id = retrieve_last_seen_id(FILE_NAME) # we get the last seen id so we can reply directly to the submitter's tweet
+    print(last_seen_id)
+    mentions = api.mentions_timeline(
+                           last_seen_id,
+                           tweet_mode='extended',
+                        include_entities=True) #this will set mentions equal to all mentions since the last seen mention
+    for mention in reversed(mentions): #we reverse mentions so that we go through the tweets in chronological order
+        last_seen_id = mention.id
+        # now update the status with the image and the lines of poetry
+        api.update_with_media(filename, status= '@'+ mention.user.screen_name + '\n' + line1 +'\n'+line2+'\n'+line3,
+                                            in_reply_to_status_id=last_seen_id)
+    store_last_seen_id(last_seen_id, FILE_NAME)
+    return
 
-while True:
+# this function will resize the submitted image so that it is more easily analysed
+def resizing():
+    image = Image.open('yourimage.png')     # open the image
+    width, height = image.size              # get the size
+    print(width, height)
+    newheight = 302.0
+    newwidth = 403.0
+    y = newheight/height
+    x = newwidth/width                      # this is so that we get a specific resolution and image size
+    print(x, y)
+    image.resize((int(width*x),int(height*y)))  # then we resize the image
+    return image
+
+
+# this is the function that does everything. It combines all the pieces and does the work.
+def ball_rolling():
     dwn_img()
-    time.sleep(150)
 
-### run up to this point with no error codes
-# but after the first time, the sample imports from rnn don't print. Not sure why.
+    newimage = resizing()
+
+    color1 = find_color_1(newimage, newimage.width, newimage.height)
+    color2 = find_color_2(newimage, newimage.width, newimage.height)
+    color3 = find_color_3(newimage, newimage.width, newimage.height)
+
+    seed1 = pick_a_word(color1)
+    seed2 = pick_a_word(color2)
+    seed3 = pick_a_word(color3)
+
+    line1 = seed_to_line(seed1, file="line1.txt")
+    line2 = seed_to_line(seed2, file="line2.txt")
+    line3 = seed_to_line(seed3, file="line3.txt")
+
+    tweet_img(filename, line1, line2, line3)
+
+
+# we have two options for running the code.
+#we can either just run the function once:
+ball_rolling()
+
+# or we can have an infinite while loop and the code will be run every 150 seconds
+#while True:
+ #   time.sleep(150)
+
+
